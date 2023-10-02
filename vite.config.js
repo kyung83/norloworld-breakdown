19e80a49d15f7942950e429e30a19ec56fd1ec01
ivoorusso <ivan@setandforget.io>
2023-10-02 19:08:31 -0300

    nav3

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/norloworld-breakdown"
+  base: process.env.NODE_ENV === 'production' ? '/norloworld-breakdown/' : '/',
 })
