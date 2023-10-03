be0e92840a123f6a72c5ec06ffe71d7c19ed1d8f
ivoorusso <129088802+ivoorusso@users.noreply.github.com>
2023-10-03 11:04:09 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ const user = {
     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 }
 const navigation = [
-  { name: 'Breakdowns', href: 'norloworld-breakdown/', current: true },
-  { name: 'Status', href: 'norloworld-breakdown/status', current: false },
+  { name: 'Breakdowns', href: '/norloworld-breakdown/', current: true },
+  { name: 'Status', href: '/norloworld-breakdown/status', current: false },
 ]
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
